77cb9b42646bee38b195be041e64ab8e9795199e
Bren Briggs <code@fraq.io>
2022-02-18 09:48:52 -0500

    firefly // Port build scripts and docs from old repo

---
+++ charts/firefly/contrib/deploy/local/deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+set xe
+WORKDIR=$(pwd)
+CLUSTER_NAME="firefly"
+REGISTRY_NAME="firefly"
+REGISTRY_PORT="8081"
+SERVER_COUNT=1
+AGENT_COUNT=0
+FIREFLY_NAMESPACE="firefly"
+IMAGE_VERSION="rancher/k3s:v1.21.5-k3s2"
+
+# TODO: add check for k3d >= 5.0.0
+# k3s version must be 1.21.4 or better to avoid local-path permissions bug
+#k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
+k3d cluster create ${CLUSTER_NAME} \
+  -p 8090:80@loadbalancer \
+  --image ${IMAGE_VERSION} \
+  --servers ${SERVER_COUNT} \
+  --agents ${AGENT_COUNT} \
+  --kubeconfig-update-default=true \
+  --k3s-arg "--disable=traefik@server:0"
+
+# merge the kubeconfig into the default location
+k3d kubeconfig merge -d firefly
+
+# TODO: add check for helm binary. Must be v3 or better.
+echo "Installing firefly stack..."
+kubectl create namespace ${FIREFLY_NAMESPACE}
+
+# This takes a _long_ time if you opted to pull all the base images in by default
+helm install --namespace ${FIREFLY_NAMESPACE} firefly ../../../
+cd $WORKDIR
+echo "JupyterHub should be available at http://localhost:8090"
